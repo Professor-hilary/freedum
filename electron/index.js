8c4a4439a76057e97b03fed93c752ab054eb45b0
Professor-hilary <professorhilary98@outlook.com>
2025-02-13 04:21:30 +0300

    Added a sticky nav bar

--- electron/index.js
+++ electron/index.js
@@ -1,15 +1,52 @@
-const { app, BrowserWindow } = require('electron'); 
+const { error } = require('console');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
+const path = require('path');
 
 let mainWindow;
 
-app.whenReady().then(() => {
-    mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 800,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+function createWindow() {
+    try {
+        mainWindow = new BrowserWindow({
+            width: 1200,
+            height: 800,
+            webPreferences: {
+                nodeIntegration: true,
+                scrollBounce: false,
+                enableBlinkFeatures:true
+            }
+        });
 
-    mainWindow.loadURL('http://127.0.0.1:8000'); // Load Django server
+        // Load the Django app (adjust port if needed)
+        mainWindow.loadURL('http://127.0.0.1:8000'); 
+
+        // Remove menu bar
+        Menu.setApplicationMenu(null);
+
+    } catch (error) {
+        dialog.showErrorBox('Application Exception', error.message)
+    }
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+}
+
+// Handle app errors globally
+process.on('uncaughtException', (error) => {
+    dialog.showErrorBox('Uncaight Exception', error.message);
 });
+
+// Launch Electron
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+})
