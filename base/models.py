fc187b855cfa68a2956d87ca86afd858d9a7b485
Professor-hilary <professorhilary98@outlook.com>
2025-02-13 08:10:32 +0300

    Modified look and feel of home and profile pages

--- base/models.py
+++ base/models.py
@@ -17,7 +17,7 @@ class Room(models.Model):
     description = models.TextField(null=True, blank=True)
     participants = models.ManyToManyField(User, related_name='participants', blank=True)
     updated  = models.DateTimeField(auto_now=True)
-    created  = models.DateTimeField(auto_now_add=True)
+    created  = models.DateTimeField(auto_now_add=True) 
     
     class Meta:
         ordering = ['-updated', '-created']
@@ -26,7 +26,7 @@ class Room(models.Model):
         return self.name
     
 
-class Message(models.Model):
+class Message(models.Model): 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
