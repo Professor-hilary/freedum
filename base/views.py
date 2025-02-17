583e18850ba76aac56367ed65566154faa3a3290
Professor-hilary <professorhilary98@outlook.com>
2025-02-17 16:20:52 +0300

    Added topics page and more responsiveness

--- base/views.py
+++ base/views.py
@@ -194,3 +194,6 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
     
     return render(request, 'base/update_user.html', {'form': form}) 
+
+def topicsPage(request):
+    return render(request, 'base/topics.html',{}) 
