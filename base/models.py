478af908ebb9be856d87b8fa52fe8a3656a27885
Professor-hilary <professorhilary98@outlook.com>
2025-02-18 12:12:10 +0300

    Added profile picture to pages

--- base/models.py
+++ base/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-
 
 class User(AbstractUser):
     name = models.CharField(max_length=50, null=True)
     email = models.EmailField(max_length=50, null=True, unique=True)
     bio = models.TextField(max_length=250, null=True)
-    
-    # avatar =
-    
+
+    avatar = models.ImageField(null=True, default='defaults/profile.png')
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
