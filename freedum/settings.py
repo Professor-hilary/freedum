36b39836505bb3a559336835538123d7d47d4f1d
Professor-hilary <professorhilary98@outlook.com>
2025-02-20 18:59:37 +0300

    More use-less debugging

--- freedum/settings.py
+++ freedum/settings.py
@@ -144,8 +144,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    os.path.join(BASE_DIR, 'static'),  # Ensure this path exists
+    BASE_DIR / 'static',  # This should already work, make sure `static` exists in your base directory
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For `collectstatic`
