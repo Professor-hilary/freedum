cd04b18f14c0f76e4ddb4a5681fcd080809c31ce
Professor-hilary <professorhilary98@outlook.com>
2025-02-20 22:05:01 +0300

    Made a public dir just for vercel

--- freedum/settings.py
+++ freedum/settings.py
@@ -143,11 +143,11 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # This should already work, make sure `static` exists in your base directory
+    BASE_DIR / 'public/static',  # This should already work, make sure `static` exists in your base directory
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For `collectstatic`
