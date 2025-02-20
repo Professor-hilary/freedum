22015952038928abc98844f8dc9f436ef46caaa5
Professor-hilary <professorhilary98@outlook.com>
2025-02-20 16:24:10 +0300

    Added postresql db from supabase

--- freedum/settings.py
+++ freedum/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,10 +88,11 @@ WSGI_APPLICATION = 'freedum.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    """ 'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    } """
+    "default" : dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
