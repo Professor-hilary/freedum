4a5b10edb4bc537beaa29bc0d11248eac6697bfa
Professor-hilary <professorhilary98@outlook.com>
2025-02-20 18:09:34 +0300

    Added super user

--- base/models.py
+++ base/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     avatar = models.ImageField(null=True, default='defaults/profile.png')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
 
 class Topic(models.Model):
