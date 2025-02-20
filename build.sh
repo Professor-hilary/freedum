3e79f4f3978dc7b61b60a92274f66edc7d46d840
Professor-hilary <professorhilary98@outlook.com>
2025-02-20 19:37:00 +0300

    Forcing building of staticfiles in vercel again with echo

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Output to verify that the script is being executed
+echo "Running collectstatic..."
+
+# Run collectstatic and output the result
 python manage.py collectstatic --noinput
+
+# Confirm if collectstatic ran
+echo "collectstatic completed"
